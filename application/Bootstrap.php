ec7c2d0001d502f9c76eefc94771baa50c6fe5ac
Koen <info@koenhendriks.com>
2014-02-03 14:34:20 +0100

    Initial

---
+++ application/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+    protected function _initSetupBaseUrl()
+    {
+        $this->bootstrap('frontcontroller');
+        $controller = Zend_Controller_Front::getInstance();
+        $controller->setBaseUrl('/');
+    }
+
+}
+
