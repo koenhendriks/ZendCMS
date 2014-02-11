b033dbb7c86ac983b8ff2e2c74acc57164f3088c
Koen <info@koenhendriks.com>
2014-02-11 13:57:19 +0100

    Base URL fix

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,9 +5,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     protected function _initSetupBaseUrl()
     {
-        $this->bootstrap('frontcontroller');
-        $controller = Zend_Controller_Front::getInstance();
-        $controller->setBaseUrl('/');
     }
 
 
