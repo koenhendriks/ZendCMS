8c7b36e966c32fa39cd5ca107ab2ee94e3d6d4d8
Koen <info@koenhendriks.com>
2014-02-17 13:57:28 +0100

    added addContent function for database

--- application/models/DbTable/Content.php
+++ application/models/DbTable/Content.php
@@ -5,6 +5,14 @@ class Application_Model_DbTable_Content extends Zend_Db_Table_Abstract
 
     protected $_name = 'content';
 
-
+    public function addContent($titel,$text)
+    {
+        $data = array(
+            'title' => $title,
+            'content' => $content,
+            'created' => date("Y-m-d H:i:s")
+        );
+        $this->insert($data);
+    }
 }
 
