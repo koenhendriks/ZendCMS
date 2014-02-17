2c14e983c1a7bf03710b05e332f30e2fd82c9a73
Koen <info@koenhendriks.com>
2014-02-17 13:58:02 +0100

    corrected variables

--- application/models/DbTable/Content.php
+++ application/models/DbTable/Content.php
@@ -5,11 +5,11 @@ class Application_Model_DbTable_Content extends Zend_Db_Table_Abstract
 
     protected $_name = 'content';
 
-    public function addContent($titel,$text)
+    public function addContent($title,$text)
     {
         $data = array(
             'title' => $title,
-            'content' => $content,
+            'content' => $text,
             'created' => date("Y-m-d H:i:s")
         );
         $this->insert($data);
