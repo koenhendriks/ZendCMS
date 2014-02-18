604f4983dd3591b6dc2e0618a4fab42b983a5a1f
Koen <info@koenhendriks.com>
2014-02-18 12:55:05 +0100

    Created action for editing

--- application/controllers/ContentController.php
+++ application/controllers/ContentController.php
@@ -37,11 +37,49 @@ class ContentController extends Zend_Controller_Action
             {
                 $form->populate($formData);
             }
-
         }
     }
 
+    public function editAction()
+    {
+        $form = new Application_Form_Content();
+        $form->submit->setLabel('Wijzig');
+        $this->view->form = $form;
+
+        if($this->getRequest()->isPost())
+        {
+            $formData = $this->getRequest()->getPost();
+
+            if($form->isValid($formData))
+            {
+                $id = (int) $form->getValue('id');
+                $title = $form->getValue('title');
+                $text = $form->getValue('text');
+                $created = $form->getValue('created');
+
+                $courses = new Application_Model_DbTable_Content();
+                $courses->updateContent($id, $title, $text);
+                $this->_helper->redirector('index');
+            }
+            else
+            {
+                $form->populate($formData);
+            }
+        }
+        else
+        {
+            $id = $this->_getParam('id',0);
+            if($id > 0)
+            {
+                $content = new Application_Model_DbTable_Content();
+                $form->populate($content->getContent($id));
+            }
+        }
+
+    }
 
 
 }
+
+
 
