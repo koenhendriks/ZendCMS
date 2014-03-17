520fa6cb098a56999f24e91276fe85e1c41aab48
Koen <info@koenhendriks.com>
2014-03-17 13:52:26 +0100

    Registered namespace for library

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,12 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    protected function _initNamespaces()
+    {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('ZendCMS_');
+    }
+
     protected function _initSetupBaseUrl()
     {
     }
